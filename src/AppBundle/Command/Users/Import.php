82a14e8b131e7aa3d95eee056f36187753841294
Eugene Yurkevich <eugeney@playtika.com>
2017-10-03 21:30:33 +0300

    save some rows from csv

--- src/AppBundle/Command/Users/Import.php
+++ src/AppBundle/Command/Users/Import.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command\Users;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -9,12 +10,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Import extends ContainerAwareCommand
 {
+    const COLUMNS_AMOUNT_IN_CSV = 13;
+    const BATCH_ROWS = 1000;
+    const DELIMITER = ';';
+
+    private $updatedCnt = 0;
+    private $insertedCnt = 0;
+    private $processedCnt = 0;
+
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $em;
+
     protected function configure()
     {
         $this
             ->setName('users:import')
-            ->setDescription('Creates new users from CSV file.')
-            ->setHelp("This command allows you to import users from csv file.")
+            ->setDescription('Creates new user from CSV file.')
+            ->setHelp("This command allows you to import user from csv file.")
             ->addOption(
                 'csvFileName',
                 'f',
@@ -25,12 +39,117 @@ class Import extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $csvFile = $input->getOption('csvFileName');
+        $csvFilePath = $input->getOption('csvFileName');
+
+        if (!file_exists($csvFilePath)) {
+            throw new \Exception('file not found: '.$csvFilePath);
+        }
+
+        $this->readCsv($output, $csvFilePath);
+    }
+
+    private function readCsv( OutputInterface $output, $csvFilePath)
+    {
+        $doctrine = $this->getContainer()->get('doctrine');
+        $this->em = $doctrine->getManager();
 
         $output->writeln(['Start importing...']);
 
-        if (!file_exists($csvFile)) {
-            throw new \Exception('file not found: ' . $csvFile);
+        $rows = [];
+
+        $output->writeln(['Skip first line with titles']);
+        if (($handle = fopen($csvFilePath, 'r')) !== false) {
+            fgetcsv($handle);
+
+            $i = 1;
+            while (($data = fgetcsv($handle, null, self::DELIMITER)) !== false) {
+                $i++;
+                $this->processedCnt++;
+                if (count($data) != self::COLUMNS_AMOUNT_IN_CSV) {
+                    $output->writeln(['Skip line '. $i]);
+                    continue;
+                }
+
+                $rows[] = array_map('trim', $data);
+
+                if (count($rows) >= self::BATCH_ROWS) {
+                    $this->saveUsersData($rows);
+                    $rows = [];
+                    $output->writeln([date('Y-m-d H:i:s') . " Processed $this->processedCnt rows, inserted $this->insertedCnt, updated $this->updatedCnt."]);
+                }
+            }
+            fclose($handle);
         }
+
+        if ($rows) {
+            $this->saveUsersData($rows);
+        }
+
+        $output->writeln([date('Y-m-d H:i:s') . " Finally, processed $this->processedCnt rows, inserted $this->insertedCnt, updated $this->updatedCnt."]);
+    }
+
+    private function saveUsersData(array $rows)
+    {
+        if (!$rows) {
+            return;
+        }
+
+        $emails = [];
+        foreach ($rows as $row) {
+            $emails[] = $row[3];
+        }
+
+        $query = $this->em->createQueryBuilder()
+            ->select('u')
+            ->from('AppBundle:User', 'u')
+            ->where('u.email in (:emails)')
+            ->setParameter('emails', $emails)
+            ->getQuery()
+        ;
+
+        $existingUser = $query->getResult();
+        $emailToUserMap = [];
+        /** @var User $user */
+        foreach ($existingUser as $user) {
+            $emailToUserMap[$user->getEmail()] = $user;
+        }
+
+        foreach ($rows as $row) {
+            if (isset($emailToUserMap[$row[3]])) {
+                $user = $emailToUserMap[$row[3]];
+                $this->assignValues($user, $row);
+                $this->em->persist($user);
+                $this->updatedCnt++;
+                continue;
+            }
+
+            $user = new User();
+            $this->assignValues($user, $row);
+            $this->em->persist($user);
+            $this->insertedCnt++;
+        }
+        $this->em->flush();
+    }
+
+    /**
+     * @param User $user
+     * @param array $row
+     * @return User
+     */
+    private function assignValues(User $user, array $row)
+    {
+        $user->setFirstName($row[0]);
+        $user->setLastName($row[1]);
+        $user->setBirthday(new \DateTime($row[2]));
+        $user->setEmail($row[3]);
+        $user->setCity($row[4]);
+        $user->setZip($row[5]);
+        $user->setAddress($row[6]);
+        $user->setPhone($row[7]);
+        $user->setCompany($row[8]);
+        $user->setWorkCity($row[9]);
+        $user->setWorkAddress($row[10]);
+        $user->setPosition($row[11]);
+        $user->setCv($row[12]);
     }
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,438 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="date")
+     */
+    private $birthday;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zip", type="string", length=255)
+     */
+    private $zip;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company", type="string", length=255)
+     */
+    private $company;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="workCity", type="string", length=255)
+     */
+    private $workCity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="workAddress", type="string", length=255)
+     */
+    private $workAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="position", type="string", length=255)
+     */
+    private $position;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cv", type="text")
+     */
+    private $cv;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     *
+     * @return User
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set zip
+     *
+     * @param string $zip
+     *
+     * @return User
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    /**
+     * Get zip
+     *
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     *
+     * @return User
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set workCity
+     *
+     * @param string $workCity
+     *
+     * @return User
+     */
+    public function setWorkCity($workCity)
+    {
+        $this->workCity = $workCity;
+
+        return $this;
+    }
+
+    /**
+     * Get workCity
+     *
+     * @return string
+     */
+    public function getWorkCity()
+    {
+        return $this->workCity;
+    }
+
+    /**
+     * Set workAddress
+     *
+     * @param string $workAddress
+     *
+     * @return User
+     */
+    public function setWorkAddress($workAddress)
+    {
+        $this->workAddress = $workAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get workAddress
+     *
+     * @return string
+     */
+    public function getWorkAddress()
+    {
+        return $this->workAddress;
+    }
+
+    /**
+     * Set position
+     *
+     * @param string $position
+     *
+     * @return User
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * Get position
+     *
+     * @return string
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set cv
+     *
+     * @param string $cv
+     *
+     * @return User
+     */
+    public function setCv($cv)
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    /**
+     * Get cv
+     *
+     * @return string
+     */
+    public function getCv()
+    {
+        return $this->cv;
+    }
+}
+
